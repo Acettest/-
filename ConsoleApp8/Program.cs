a97cc59c220dceca20f675d601f59e82879c3786
Acettest <shengxiaomiaoyx@qq.com>
2018-06-02 17:09:37 +0800

    在群里发生的一些争论，几乎出现的所有人都算错了，虽然简单，但很有意思，也反映了一些基础问题

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
+            var a = new A();
+            var b = new B();
+            a.fun1(1);//1
+            a.fun2(b);//2,5
+
+            b.fun1(1);//2
+            b.fun2(a);//1,6
         }
+
+        public class A
+        {
+            public virtual void fun1(int i)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            public void fun2(A a)
+            {
+                a.fun1(1);
+                fun1(5);
+            }
+        }
+
+        public class B : A
+        {
+            public override void fun1(int i)
+            {
+                base.fun1(i+1);
+            }
+        }
+
+
     }
 }
